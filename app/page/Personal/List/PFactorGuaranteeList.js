0a041e2cff32f3813c9be6fa9161c733375f975d
咪酱 <sevenbb2@sina.com>
2019-10-05 21:05:30 +0800

    修改表单记录样式+添加进度查看

--- app/page/Personal/List/PFactorGuaranteeList.js
+++ app/page/Personal/List/PFactorGuaranteeList.js
@@ -13,9 +13,10 @@ import moment from 'moment'
 import { Col, Row, Grid} from 'react-native-easy-grid'
 import { connect } from 'react-redux'
 import { NavigationBar, NavigationPage } from 'teaset'
-import { Touchable, Button, IconFont, NavBar } from '../../../components'
+import { Icon, Steps, WingBlank } from '@ant-design/react-native';
+import { Touchable, ModalView, IconFont, NavBar } from '../../../components'
 import { NavigationActions, commonStyle } from '../../../utils'
-
+const Step = Steps.Step;
 
 // 取得屏幕的宽高Dimensions
 const { width, height } = Dimensions.get('window')
@@ -26,6 +27,8 @@ class PFactorGuaranteeList extends NavigationPage {
   constructor(props) {
     super(props)
     this.state = {
+      isModalShow:false,
+      modalContent: null,
     }
   }
 
@@ -38,16 +41,33 @@ class PFactorGuaranteeList extends NavigationPage {
     this.props.dispatch(NavigationActions.navigate({ routeName, params }))
   }
 
-  _renderItemView({item, index}){
-    return(
+  handleCheckDetail= item => {
+    console.log('========handleCheckDetail============================');
+    console.log(item);
+    console.log('====================================');
+    this.setState({ isModalShow: true, modalContent: item.procedureDetails})
+  }
+  hideModal = () => {
+    this.setState({ isModalShow: false })
+  }
+  _renderItemView = ({ item, index }) => (
+    // return(
       <View style={styles.block}>
         <View style={styles.blockTitle}>
           <View style={styles.leftLabel}>
-            <Text>{item.contact}</Text>
+            <Text style={styles.leftLabel_name}>{item.contact}</Text>
           </View>
-          <View style={styles.rightLabel}>
-            <Text>查看申请进度>>></Text>
-          </View>
+          <Touchable onPress={()=>this.handleCheckDetail(item)}>
+            <View style={styles.rightLabel} >
+              <Text>查看申请进度 
+              <IconFont
+                  name="&#xe6eb;"
+                  size={commonStyle.h2Size}
+                  style={{ color: commonStyle.h2Color }}
+                />
+              </Text>
+            </View>
+          </Touchable> 
         </View>
         <View style={styles.blockContent}>
           <Text>{item.content}</Text>
@@ -57,15 +77,16 @@ class PFactorGuaranteeList extends NavigationPage {
             <Text>{item.enterpriseName}</Text>
           </View>
           <View style={styles.rightLabel}>
-            <Text>{moment(item.createdTime).format('YYYY-MM-DD')}</Text>
+            <Text style={styles.rightLabel_time}>{moment(item.createdTime).format('YYYY-MM-DD')}</Text>
           </View>
         </View>
       </View>
     )
-  }
+  // }
 
 
   renderPage() {
+    const { modalContent } = this.state
     const {userinfo} = this.props.user
     const {
       factorGuarantees,
@@ -81,6 +102,40 @@ class PFactorGuaranteeList extends NavigationPage {
           showsVerticalScrollIndicator={false}
           enabled
         />
+        <ModalView
+          isShow={this.state.isModalShow}
+          hideModal={this.hideModal}
+          contentHeight={360}
+          contentStyle={styles.modal_content}
+          title="申请进度"
+        >
+          <ScrollView
+            style={{ flex: 1 }}
+            automaticallyAdjustContentInsets={false}
+            showsHorizontalScrollIndicator={false}
+            showsVerticalScrollIndicator={false}
+          >
+            <View style={{ marginTop: 60 }}>
+              <WingBlank size="lg">
+                {modalContent&&modalContent.length>0?<Steps size="small" current={1} direction="vertical">
+                  {modalContent.map((item, index) => (
+                    <Step
+                      current={0}
+                      key={index}
+                      title={
+                        <View>
+                          <Text>{item.content}</Text>
+                          <Text>{item.procTime}</Text>
+                        </View>
+                      }
+                      status='finish'
+                    />
+                  ))}
+                </Steps>:<Text>抱歉, 暂无进度</Text>}
+              </WingBlank>
+            </View>
+        </ScrollView>
+      </ModalView>
       </View>
     )
   }
@@ -91,21 +146,21 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   block:{
-    marginTop: 10,
-    marginLeft: 4,
-    marginRight: 4,
+    // marginTop: 10,
     flex: 1,
-    borderRadius: 4,
-    borderWidth:1,
-    borderColor: '#999',
+    // borderRadius: 4,
+    paddingHorizontal:5,
+    paddingVertical:8,
+    borderBottomWidth:1,
+    borderBottomColor: '#d2d2d2',
     backgroundColor: '#fff',
   },
   blockTitle:{
     flexDirection: 'row',
     justifyContent: 'space-between',
     padding:4,
-    borderBottomWidth:1,
-    borderBottomColor: '#bbb',
+    // borderBottomWidth:1,
+    // borderBottomColor: '#eee',
   },
   blockContent:{
     padding: 4,
@@ -114,14 +169,25 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-between',
     padding:4,
-    borderTopWidth:1,
-    borderTopColor: '#bbb',
+    // borderTopWidth:1,
+    // borderTopColor: '#eee',
   },
   leftLabel:{
     alignSelf: 'flex-start',
   },
+  leftLabel_name: {
+    fontSize:20,
+    color:'#333',
+  },
   rightLabel:{
     alignSelf: 'flex-end',
+    display:"flex",
+    flexDirection:'row',
+    justifyContent:'center',
+    alignItems:'center'
+  },
+  rightLabel_time: {
+    color:'#e0161b'
   }
 })
 
